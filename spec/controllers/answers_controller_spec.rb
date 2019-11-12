5fab58d8224f792a7a70a268b84c9aadf571bedc
celtic93 <egorka80@list.ru>
2019-11-12 07:05:33 +0300

    Fixed desciption in specs

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -55,7 +55,7 @@ RSpec.describe AnswersController, type: :controller do
     end
 
     context 'with invalid attributes' do
-      it 'does not save the question' do
+      it 'does not save the answer' do
         expect { post :create, params: { question_id: question,
                                          answer: attributes_for(:answer, :invalid) } }.to_not change(question.answers, :count)
       end
