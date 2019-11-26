a1e1f5a8a87bdb73c9cfb12eaed920b86cc39ea6
celtic93 <egorka80@list.ru>
2019-11-26 03:36:54 +0300

    User can edit question with ajax with specs

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -1,41 +1,46 @@
 require 'rails_helper'
 
-feature 'User can edit answer' do
+feature 'User can edit question' do
   given!(:user) { create(:user) }
   given!(:question) { create(:question) }
-  given!(:answer) { create(:answer, question: question) }
 
   describe 'Authenticated user tryes to edit' do
-    scenario 'his answer', js: true do
-      sign_in(answer.user)
+    scenario 'his question', js: true do
+      sign_in(question.user)
       visit question_path(question)
 
-      click_on 'Edit'
+      click_on 'Edit question'
 
-      within '.answers' do
-        fill_in 'Body', with: 'edited answer'
+      within '.question' do
+        fill_in 'Title', with: 'edited question title'
+        fill_in 'Body', with: 'edited question body'
         click_on 'Save'
 
-        expect(page).to_not have_content answer.body
-        expect(page).to have_content 'edited answer'
+        expect(page).to_not have_content question.title
+        expect(page).to_not have_content question.body
+        expect(page).to have_content 'edited question title'
+        expect(page).to have_content 'edited question body'
         expect(page).to_not have_selector 'textarea'
       end
     end
 
     scenario 'his answer with errors', js: true do
-      sign_in(answer.user)
+      sign_in(question.user)
       visit question_path(question)
 
-      click_on 'Edit'
+      click_on 'Edit question'
 
-      within '.answers' do
+      within '.question' do
+        fill_in 'Title', with: ''
         fill_in 'Body', with: ''
         click_on 'Save'
         
-        expect(page).to have_content answer.body
+        expect(page).to have_content question.title
+        expect(page).to have_content question.body
         expect(page).to have_selector 'textarea'
       end
 
+      expect(page).to have_content "Title can't be blank"
       expect(page).to have_content "Body can't be blank"
     end
 
