1729d5185c8ef67039a325b97f78392e612a8d06
celtic93 <egorka80@list.ru>
2019-12-11 06:18:58 +0300

    User can add several links when editing question with specs

--- spec/features/question/add_link_spec.rb
+++ spec/features/question/add_link_spec.rb
@@ -4,6 +4,7 @@ feature 'User can add links to question' do
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/celtic93/c30f65926746100a74f911ade581199d' }
   given(:google_url) { 'https://google.com' }
+  given!(:question) { create(:question) }
 
   scenario 'User adds links when asks question', js: true do
     sign_in(user)
@@ -26,5 +27,31 @@ feature 'User can add links to question' do
 
     expect(page).to have_link 'My gist', href: gist_url
     expect(page).to have_link 'Google', href: google_url
+  end
+
+  scenario 'User adds links when edits question', js: true do
+    sign_in(question.user)
+    visit question_path(question)
+
+    click_on 'Edit question'
+
+    within '.question' do
+      click_on 'Add link'
+      
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'Url', with: gist_url
+
+      click_on 'Add link'
+
+      within all('.new-link').last do
+        fill_in 'Link name', with: 'Google'
+        fill_in 'Url', with: google_url
+      end
+
+      click_on 'Save'
+
+      expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'Google', href: google_url
+    end
   end
 end
