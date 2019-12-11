d5e579e1ddd51934a3c9eb158122264a4995c231
celtic93 <egorka80@list.ru>
2019-12-11 05:21:52 +0300

    User can add several links when creating answer with specs

--- spec/features/answer/add_link_spec.rb
+++ spec/features/answer/add_link_spec.rb
@@ -4,6 +4,7 @@ feature 'User can add links to answer' do
   given(:user) { create(:user) }
   given(:question) { create(:question) }
   given(:gist_url) { 'https://gist.github.com/celtic93/c30f65926746100a74f911ade581199d' }
+  given(:google_url) { 'https://google.com' }
 
   scenario 'User adds link when asks answer', js: true do
     sign_in(user)
@@ -14,10 +15,18 @@ feature 'User can add links to answer' do
     fill_in 'Link name', with: 'My gist'
     fill_in 'Url', with: gist_url
 
+    click_on 'Add link'
+
+    within all('.new-link').last do
+      fill_in 'Link name', with: 'Google'
+      fill_in 'Url', with: google_url
+    end
+
     click_on 'Answer'
 
     within '.answers' do
       expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'Google', href: google_url
     end
   end
 end
