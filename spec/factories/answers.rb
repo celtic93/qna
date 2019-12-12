370c7ba4f197b63238e94a01ca2752442a9df265
celtic93 <egorka80@list.ru>
2019-12-12 04:33:18 +0300

    User can delete links when editing answer with specs

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -12,5 +12,11 @@ FactoryBot.define do
     trait :with_file do
       files { fixture_file_upload("#{Rails.root}/spec/spec_helper.rb") }
     end
+
+    trait :with_link do
+      after(:create) do |question|
+        create :link, linkable: question
+      end
+    end
   end
 end
