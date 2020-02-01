51fe316cf8676e3905e5675d500b648f8b536cdf
celtic93 <egorka80@list.ru>
2020-02-01 20:23:02 +0300

    Added questions list api

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ gem 'omniauth-vkontakte'
 gem 'cancancan'
 
 gem 'doorkeeper'
+gem 'active_model_serializers', '~> 0.10'
+gem 'oj'
 
 group :development, :test do
   gem 'pry'
---
+++ spec/api/v1/questions_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+describe 'Profiles API', type: :request do
+  let(:headers) { { 'CONTENT_TYPE' => 'application/json',
+                    'ACCEPT' => 'application/json' } }
+
+  describe 'GET api/v1/questions' do
+    it_behaves_like 'API authorizable' do
+      let(:method) { :get }
+      let(:api_path) { '/api/v1/questions' }
+    end
+
+    context 'authorized' do
+      let(:access_token) { create(:access_token) }
+      let!(:questions) { create_list(:question, 2) }
+      let(:question) { questions.first }
+      let(:question_response) { json['questions'].first }
+      let!(:answers) { create_list(:answer, 2, question: question) }
+
+      before { get '/api/v1/questions', params: { access_token: access_token.token },
+                                        headers: headers }
+
+      it 'returns 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'returns list of questions' do
+        expect(json['questions'].size).to eq 2
+      end
+
+      it 'returns all public fields' do
+        %w(id title body created_at updated_at).each do |attr|
+          expect(question_response[attr]).to eq question.send(attr).as_json
+        end
+      end
+
+      it 'contains user object' do
+        expect(question_response['user']['id']).to eq question.user.id
+      end
+
+      it 'contains short title' do
+        expect(question_response['short_title']).to eq question.title.truncate(10)
+      end
+
+      describe 'answers' do
+        let(:answer) { answers.first }
+        let(:answer_response) { question_response['answers'].first }
+
+        it 'returns list of answers' do
+          expect(question_response['answers'].size).to eq 2
+        end
+
+        it 'returns all public fields' do
+          %w(id body user_id created_at updated_at best).each do |attr|
+            expect(answer_response[attr]).to eq answer.send(attr).as_json
+          end
+        end
+      end
+    end
+  end
+end
