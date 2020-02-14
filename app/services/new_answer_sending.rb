adfa6f5c4b57344c059d97bbc9382f9dcbefeb52
celtic93 <egorka80@list.ru>
2020-02-14 18:39:45 +0300

    Added fixes 2

--- app/services/new_answer_sending.rb
+++ app/services/new_answer_sending.rb
@@ -1,8 +1,6 @@
 class Services::NewAnswerSending
   def send_answer(answer)
-    question = Question.where(id: answer.question).includes(subscriptions: :user).first
-
-    question.subscriptions.find_each do |subscription|
+    answer.question.subscriptions.includes(:user).find_each do |subscription|
       user = subscription.user
       NewAnswerMailer.send_answer(user, answer).deliver_later unless user.is_author?(answer)
     end
--- spec/services/daily_digest_spec.rb
+++ spec/services/daily_digest_spec.rb
@@ -3,8 +3,19 @@ require 'rails_helper'
 RSpec.describe Services::DailyDigest do
   let(:users) { create_list(:user, 2) }
 
-  it 'sends daily digest to all users' do
-    users.each {|user| expect(DailyDigestMailer).to receive(:digest).with(user).and_call_original}
-    subject.send_digest
+  context 'with yesterday questions' do
+    let(:question) { create(:question, created_at: Date.yesterday) }
+
+    it 'sends daily digest to all users' do
+      users.each {|user| expect(DailyDigestMailer).to receive(:digest).with(user).and_call_original}
+      subject.send_digest
+    end
+  end
+
+  context 'with no yesterday questions' do
+    it 'does not send daily digest to all users' do
+      users.each {|user| expect(DailyDigestMailer).to_not receive(:digest).with(user)}
+      subject.send_digest
+    end
   end
 end
