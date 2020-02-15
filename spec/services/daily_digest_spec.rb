999b65bd03fb2d106b464295102c242e3baa3612
celtic93 <egorka80@list.ru>
2020-02-15 18:33:35 +0300

    Added spec fix

--- spec/services/daily_digest_spec.rb
+++ spec/services/daily_digest_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Services::DailyDigest do
   let(:users) { create_list(:user, 2) }
 
   context 'with yesterday questions' do
-    let(:question) { create(:question, created_at: Date.yesterday) }
+    let!(:question) { create(:question, created_at: Date.yesterday, user: users[0]) }
 
     it 'sends daily digest to all users' do
       users.each {|user| expect(DailyDigestMailer).to receive(:digest).with(user).and_call_original}
